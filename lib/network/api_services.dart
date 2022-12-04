01d0f179832d443719bc3ee39feea4251c25dbd8
Mohammad Mustafa <mustafa@sherpa-tech.com>
2022-12-04 14:43:50 +0545

    WIP

--- lib/network/api_services.dart
+++ lib/network/api_services.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:meroapp/model/content_model.dart';
 
 class ApiServices {
-  Future getContent() async {
+  static Future getContent() async {
     final url =
         Uri.parse("https://meropadai.com/wp-json/wp/v2/posts/?embed=true");
     try {
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:meroapp/network/api_services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -17,46 +16,63 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: ListView.separated(
-          itemCount: 10,
-          separatorBuilder: (context, index) => SizedBox(
-            height: 10,
-          ),
-          itemBuilder: (context, index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  color: Colors.grey,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Text(
-                    "Title",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+      body: FutureBuilder(
+          future: ApiServices.getContent(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: ListView.separated(
+                  itemCount: 10,
+                  separatorBuilder: (context, index) => SizedBox(
+                    height: 10,
                   ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 200,
+                          width: double.infinity,
+                          color: Colors.grey,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            "Title",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            "Small Description",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Text(
-                    "Small Description",
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if(snapshot.hasError) {
+              return Center(
+                child: Container(
+                  child: Text("Err"),
                 ),
-              ],
-            );
-          },
-        ),
-      ),
+              );
+            }
+            return Container();
+          }),
     );
   }
 }
