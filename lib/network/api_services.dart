cee8546875ca2c1d8f3c2b16aecfc044b523476a
Mohammad Mustafa <mustafa@sherpa-tech.com>
2022-12-04 14:26:52 +0545

    WIP

---
+++ lib/network/api_services.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:meroapp/model/content_model.dart';
+
+class ApiServices {
+  Future getContent() async {
+    final url =
+        Uri.parse("https://meropadai.com/wp-json/wp/v2/posts/?embed=true");
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final decodedResult = jsonDecode(response.body);
+        List<ContentModel> contents =  List.from(decodedResult)
+            .map(
+              (e) => ContentModel.fromJson(e),
+            )
+            .toList();
+        return contents;
+      }
+
+    } catch (e) {
+      throw e;
+    }
+  }
+}
