6f6479b4fe3a476f610a57a4c3fa4e9b25921cf0
Mohammad Mustafa <mustafa@sherpa-tech.com>
2022-12-04 12:17:35 +0545

    WIP

---
+++ android/app/src/main/kotlin/com/example/meroapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meroapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Meropadai",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.separated(
+        itemCount: 10,
+        separatorBuilder: (context, index) => SizedBox(height: 10,),
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.grey,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
