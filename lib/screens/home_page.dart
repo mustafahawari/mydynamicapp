6a04826f547ce7c0e28127e74ab648d229e55220
Mohammad Mustafa <mustafa@sherpa-tech.com>
2022-12-04 16:24:30 +0545

    WIP

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -17,98 +17,192 @@ class HomePage extends StatelessWidget {
             color: Colors.black,
           ),
         ),
+        centerTitle: true,
         backgroundColor: Colors.white,
-        actions: [
-          DrawerWidget()
-        ],
+        actions: [DrawerWidget()],
       ),
-      drawer: Drawer(),
-      body: FutureBuilder(
-          future: ApiServices.getContent(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<ContentModel> result = snapshot.data;
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: ListView.separated(
-                  itemCount: result.length,
-                  separatorBuilder: (context, index) => SizedBox(
-                    height: 10,
-                  ),
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailsPage(
-                              details: result[index].details!,
-                              imageUrl: result[index].imageUrl!,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 200,
-                            width: double.infinity,
-                            color: Colors.grey,
-                            child: Image.network(
-                              result[index].imageUrl!,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Text(
-                              result[index].title!,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Html(data: result[index].shortDescription!)
-                              // Text(
-                              //   result[index].shortDescription!,
-                              //   style: TextStyle(
-                              //     fontSize: 16,
-                              //   ),
-                              // ),
-                              ),
-                        ],
-                      ),
-                    );
+      drawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
                   },
+                  icon: Icon(
+                    Icons.close,
+                  ),
                 ),
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Container(
-                  child: Text("Err"),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
                 ),
-              );
-            }
-            return Container();
-          }),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "See 2079",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "IOM Entrance",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "C-MAT Entrance",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "CSIT- Entrance",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+            future: ApiServices.getContent(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<ContentModel> result = snapshot.data;
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ListView.separated(
+                    itemCount: result.length,
+                    separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsPage(
+                                details: result[index].details!,
+                                imageUrl: result[index].imageUrl!,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 200,
+                              width: double.infinity,
+                              color: Colors.grey,
+                              child: Image.network(
+                                result[index].imageUrl!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                result[index].title!,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child:
+                                    Html(data: result[index].shortDescription!)
+                                // Text(
+                                //   result[index].shortDescription!,
+                                //   style: TextStyle(
+                                //     fontSize: 16,
+                                //   ),
+                                // ),
+                                ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Container(
+                    child: Text("Err"),
+                  ),
+                );
+              }
+              return Container();
+            }),
+      ),
     );
   }
 }
+
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: () {
-      Scaffold.of(context).openDrawer();
-    }, icon: Icon(Icons.menu, color: Colors.black,));
+    return IconButton(
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+        icon: Icon(
+          Icons.menu,
+          color: Colors.black,
+        ));
   }
 }
